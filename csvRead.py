0351f9b4d73c9469fe58acf6140916ed71c6a91a
djdcann <djdcann@gmail.com>
2018-01-25 11:17:12 -0500

    updates

--- csvRead.py
+++ csvRead.py
@@ -1,6 +1,26 @@
 import csv
 
-with open('insects.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print row
+def read_csv(path):
+    with open(path) as csvfile:
+        return csv.DictReader(csvfile)
+
+# OLD METHOD
+# @runtime_efficiency.profile
+# def read_csv2(path):
+#     csv = open(path, 'r')
+#     data = []
+#     keys = []
+#     # collect keys from 1st line and init data
+#     for i in csv.readline().rstrip().split(','):
+#         keys.append(i)
+#     # fill data with values
+#     for i in csv.readlines():
+#         obj = {}
+#         for key, val in zip(keys, i.rstrip().split(',')):
+#             obj[key] = val
+#         data.append(obj)
+#
+#     return data
+
+
+#runtime_efficiency.print_prof_data()
