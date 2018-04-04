a36e6ecec98245d5d6c4fc19737e35ea00f551c2
djdcann <djdcann@gmail.com>
2018-04-04 11:25:42 -0400

    update

--- hw.py
+++ hw.py
@@ -29,6 +29,7 @@ list = [p1,p2]
 H = entropy(list)
 print H
 issmooth = H - 0.5*(entropy([0.75, 0.25]) + entropy([0.5,0.5]))
+print issmooth
 print gini(list)
 
 H2 = entropy([0.75, 0.25])
@@ -37,3 +38,9 @@ print 'Is Smelly: ', H2 - 0.25*entropy([1.0]) - 0.75*entropy([1.0])
 print 'Is Spotted: ', H2 - 0.75*entropy([1.0/3,2.0/3]) - 0.25*entropy([1.0])
 #choose is smooth first
 
+print entropy([0.2,0.11,0.5,0.06,0.04,0.09])
+print entropy([0.4,0.6]) - 0.6*entropy([1.0/3,2.0/3]) - 0.4*entropy([0.5,0.5])
+print entropy([0.4,0.6]) - 0.8*entropy([0.25,0.75])
+print entropy([0.4,0.6]) - 0.6*entropy([1.0/3,2.0/3]) - 0.4
+print entropy([1.0/3,2.0/3])
+print entropy([0.4,0.6])
